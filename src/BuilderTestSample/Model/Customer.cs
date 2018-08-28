5ebf87cf292be3f4860570ada65d049d0835e9b4
Steve Smith <steve@kentsmiths.com>
2018-08-28 08:57:21 -0400

    Adding constructor

--- src/BuilderTestSample/Model/Customer.cs
+++ src/BuilderTestSample/Model/Customer.cs
@@ -6,6 +6,11 @@ namespace BuilderTestSample.Model
 {
     public class Customer
     {
+        public Customer(int id)
+        {
+            Id = id;
+        }
+        
         public int Id { get; private set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
